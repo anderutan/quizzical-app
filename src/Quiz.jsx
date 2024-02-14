df2cc0d57f0784cd12460a35d3891f17671953f9
anderu <andrewtan1310@gmail.com>
2024-02-14 16:10:02 +0800

    remain the radio button as :checked no work
    when the radio button is checked and the background color won't change

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,24 +1,44 @@
 import { decode } from 'html-entities';
+import { useState } from 'react';
 
 export default function Quiz(props) {
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+
+  console.log(selectedAnswers);
+
   function handleSubmit(e) {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
     console.log(formJson);
+    console.log('Selected answers: ', selectedAnswers);
   }
 
   function qnaList() {
     const qna = props.quizQuestions.map((quiz, index) => {
       const question = decode(quiz.question);
       const options = quiz.shuffledAnswers.map((option, optionIndex) => (
-        <label key={optionIndex} className='qna-answer'>
+        <label
+          key={optionIndex}
+          htmlFor={`option${index}-${optionIndex}`}
+          className='qna-answer radio-btn'
+        >
           <input
             type='radio'
             name={`option${index}`}
             value={option}
             className='radio-btn'
+            id={`option${index}-${optionIndex}`}
+            checked={selectedAnswers[index] === option}
+            onChange={(e) => {
+              if (e.target.name === `option${index}`) {
+                setSelectedAnswers({
+                  ...selectedAnswers,
+                  [index]: e.target.value,
+                });
+              }
+            }}
           />
           {option}
         </label>
